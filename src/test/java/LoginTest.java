1baf20d87a8511033309c28755cf109f4b5251df
DieroKreator <qtechwired@gmail.com>
2024-10-28 19:43:49 -0300

    Added the backbone for tests

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,64 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+
+public class LoginTest {
+    
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+
+    }
+
+    @Test
+    public void logarComSucesso() {
+
+    }
+
+    @Test
+    public void logarCamposEmBranco() {
+        
+    }
+
+    @Test
+    public void logarUsuarioValidoSenhaEmBranco() {
+        
+    }
+
+    @Test
+    public void logarUsuarioInvalidoSenhaEmBranco() {
+        
+    }
+
+    @Test
+    public void logarUsuarioEmBrancoSenhaValida() {
+        
+    }
+
+    @Test
+    public void logarUsuarioEmBrancoSenhaInvalida() {
+        
+    }
+
+    @Test
+    public void logarUsuarioInvalidoSenhaValida() {
+        
+    }
+
+    @Test
+    public void logarUsuarioValidoSenhaInvalida() {
+        
+    }
+
+    @Test
+    public void logarDadosInvalidos() {
+        
+    }
+}
