0fc0bfd720110ce080c1f6bdf935010897ad3d31
DieroKreator <qtechwired@gmail.com>
2024-10-28 20:43:37 -0300

    Added steps for 1st test

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,7 +1,12 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginTest {
     
@@ -9,17 +14,27 @@ public class LoginTest {
 
     @BeforeEach
     public void setUp() {
-
+        driver = new ChromeDriver(); 
+        driver.get("https://the-internet.herokuapp.com/login");
+        driver.manage().window().maximize();
+        assertEquals("Login Page", 
+            driver.findElement(By.cssSelector("h2")).getText());
     }
 
     @AfterEach
     public void tearDown() {
-
+        driver.quit();
     }
 
     @Test
     public void logarComSucesso() {
+        WebElement usernameField = driver.findElement(By.id("username"));
+        WebElement passwordField = driver.findElement(By.id("password"));
+        WebElement loginBtn = driver.findElement(By.cssSelector("button.radius"));
 
+        usernameField.sendKeys("tomsmith");
+        passwordField.sendKeys("SuperSecretPassword!");
+        loginBtn.click();
     }
 
     @Test
