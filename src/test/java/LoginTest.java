400eddf7e671fc7a4ad5700e33d74791f1faeaa2
DieroKreator <qtechwired@gmail.com>
2024-10-28 22:54:46 -0300

    Added scripts for negative tests

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -9,16 +9,16 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginTest {
-    
+
     private WebDriver driver;
 
     @BeforeEach
     public void setUp() {
-        driver = new ChromeDriver(); 
+        driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/login");
         driver.manage().window().maximize();
-        assertEquals("Login Page", 
-            driver.findElement(By.cssSelector("h2")).getText());
+        assertEquals("Login Page",
+                driver.findElement(By.cssSelector("h2")).getText());
     }
 
     @AfterEach
@@ -28,6 +28,7 @@ public class LoginTest {
 
     @Test
     public void logarComSucesso() {
+
         WebElement usernameField = driver.findElement(By.id("username"));
         WebElement passwordField = driver.findElement(By.id("password"));
         WebElement loginBtn = driver.findElement(By.cssSelector("button.radius"));
@@ -35,45 +36,108 @@ public class LoginTest {
         usernameField.sendKeys("tomsmith");
         passwordField.sendKeys("SuperSecretPassword!");
         loginBtn.click();
+
+        assertEquals("You logged into a secure area!\n" + //
+                "×", driver.findElement(By.id("flash")).getText());
     }
 
     @Test
     public void logarCamposEmBranco() {
-        
+
+        WebElement loginBtn = driver.findElement(By.cssSelector("button.radius"));
+        loginBtn.click();
+
+        assertEquals("Your username is invalid!\n" + //
+                        "×", driver.findElement(By.id("flash")).getText());
     }
 
     @Test
     public void logarUsuarioValidoSenhaEmBranco() {
-        
+        WebElement usernameField = driver.findElement(By.id("username"));
+        WebElement loginBtn = driver.findElement(By.cssSelector("button.radius"));
+
+        usernameField.sendKeys("tomsmith");
+        loginBtn.click();
+
+        assertEquals("Your password is invalid!\n" + //
+                        "×", driver.findElement(By.id("flash")).getText());
     }
 
     @Test
     public void logarUsuarioInvalidoSenhaEmBranco() {
-        
+        WebElement usernameField = driver.findElement(By.id("username"));
+        WebElement loginBtn = driver.findElement(By.cssSelector("button.radius"));
+
+        usernameField.sendKeys("haskhflkafakjl");
+        loginBtn.click();
+
+        assertEquals("Your username is invalid!\n" + //
+                        "×", driver.findElement(By.id("flash")).getText());
     }
 
     @Test
     public void logarUsuarioEmBrancoSenhaValida() {
-        
+        WebElement passwordField = driver.findElement(By.id("password"));
+        WebElement loginBtn = driver.findElement(By.cssSelector("button.radius"));
+
+        passwordField.sendKeys("SuperSecretPassword!");
+        loginBtn.click();
+
+        assertEquals("Your username is invalid!\n" + //
+                        "×", driver.findElement(By.id("flash")).getText());
     }
 
     @Test
     public void logarUsuarioEmBrancoSenhaInvalida() {
-        
+        WebElement passwordField = driver.findElement(By.id("password"));
+        WebElement loginBtn = driver.findElement(By.cssSelector("button.radius"));
+
+        passwordField.sendKeys("!dwidjiiwjdiwj");
+        loginBtn.click();
+
+        assertEquals("Your username is invalid!\n" + //
+                        "×", driver.findElement(By.id("flash")).getText());
     }
 
     @Test
     public void logarUsuarioInvalidoSenhaValida() {
-        
+        WebElement usernameField = driver.findElement(By.id("username"));
+        WebElement passwordField = driver.findElement(By.id("password"));
+        WebElement loginBtn = driver.findElement(By.cssSelector("button.radius"));
+
+        usernameField.sendKeys("tomsmsjhdjsdhith");
+        passwordField.sendKeys("SuperSecretPassword!");
+        loginBtn.click();
+
+        assertEquals("Your username is invalid!\n" + //
+                        "×", driver.findElement(By.id("flash")).getText());
     }
 
     @Test
     public void logarUsuarioValidoSenhaInvalida() {
-        
+        WebElement usernameField = driver.findElement(By.id("username"));
+        WebElement passwordField = driver.findElement(By.id("password"));
+        WebElement loginBtn = driver.findElement(By.cssSelector("button.radius"));
+
+        usernameField.sendKeys("tomsmith");
+        passwordField.sendKeys("jdhdhhd!");
+        loginBtn.click();
+
+        assertEquals("Your password is invalid!\n" + //
+                        "×", driver.findElement(By.id("flash")).getText());
     }
 
     @Test
     public void logarDadosInvalidos() {
-        
+        WebElement usernameField = driver.findElement(By.id("username"));
+        WebElement passwordField = driver.findElement(By.id("password"));
+        WebElement loginBtn = driver.findElement(By.cssSelector("button.radius"));
+
+        usernameField.sendKeys("tomsm0s98f98ith");
+        passwordField.sendKeys("jdhdhhd!");
+        loginBtn.click();
+
+        assertEquals("Your username is invalid!\n" + //
+                        "×", driver.findElement(By.id("flash")).getText());
     }
 }
